8878680b567e1dbf029279b3a5fbe4940e257371
Yue Gao <hologerry@gmail.com>
2023-09-06 11:23:35 +0800

    update

---
+++ SwinDRNet/eval_real_test.sh
@@ -0,0 +1,1 @@
+python inference.py --output_dir results/real_test --train_data_path /scratch/data/DREDS/DREDS-CatKnown/train/ --val_data_path /home/yuegao/Bot/data/RealTest --val_data_type real
---
+++ SwinDRNet/eval_std_known.sh
@@ -0,0 +1,1 @@
+python inference.py --output_dir results/STD_CatKnown --train_data_path /scratch/data/DREDS/DREDS-CatKnown/train/ --val_data_path /home/yuegao/Bot/data/DREDS/STD-CatKnown --val_data_type real
