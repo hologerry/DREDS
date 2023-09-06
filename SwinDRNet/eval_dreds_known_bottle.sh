91c1ec907d407e784495d91ac8aac341521ba49a
Yue Gao <hologerry@gmail.com>
2023-09-06 12:58:56 +0800

    update

--- SwinDRNet/eval_dreds_known_bottle.sh
+++ SwinDRNet/eval_dreds_known_bottle.sh
@@ -1,6 +1,6 @@
 python inference.py \
     --output_dir results/DREDS_CatKnown \
-    --train_data_path /D_data/Bot/data/DREDS/DREDS-CatKnown/train/ \
-    --val_data_path /D_data/Bot/data/DREDS/DREDS-CatKnown/test \
+    --train_data_path /home/yuegao/Bot/data/DREDS/DREDS-CatKnown/train/ \
+    --val_data_path /home/yuegao/Bot/data/DREDS/DREDS-CatKnown/test \
     --val_data_type sim \
     --val_object_type bottle
